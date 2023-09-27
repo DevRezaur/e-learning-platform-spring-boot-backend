069344c471a4731a197d364bda9e3268acda773a
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-09-27 10:56:18 +0600

    Updated CommonConstant.java

--- common-module/src/main/java/com/devrezaur/common/module/constant/CommonConstant.java
+++ common-module/src/main/java/com/devrezaur/common/module/constant/CommonConstant.java
@@ -3,6 +3,7 @@ package com.devrezaur.common.module.constant;
 public class CommonConstant {
 
     public static final String REQUEST_ID = "Request-Id";
+    public static final String CONTENT_TYPE_HEADER_KEY = "Content-Type";
 
     private CommonConstant() {
     }
