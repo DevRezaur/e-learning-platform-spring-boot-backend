f942310682ee86bc17a8534c91da4c27a343475d
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-09-27 11:31:19 +0600

    Updated content-delivery-service (Removed DB support)

--- content-delivery-service/src/main/java/com/devrezaur/content/delivery/module/controller/ContentController.java
+++ content-delivery-service/src/main/java/com/devrezaur/content/delivery/module/controller/ContentController.java
@@ -2,17 +2,17 @@ package com.devrezaur.content.delivery.module.controller;
 
 import com.devrezaur.common.module.model.CustomHttpResponse;
 import com.devrezaur.common.module.util.ResponseBuilder;
-import com.devrezaur.content.delivery.module.model.Content;
 import com.devrezaur.content.delivery.module.service.ContentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/content")
@@ -26,20 +26,14 @@ public class ContentController {
 
     @PostMapping(consumes = "multipart/form-data")
     public ResponseEntity<CustomHttpResponse> saveContents(@RequestParam MultipartFile[] contents) {
-        List<UUID> contentIds;
+        List<String> urlList;
         try {
-            contentIds = contentService.saveContents(contents);
+            urlList = contentService.saveContents(contents);
         } catch (Exception ex) {
             return ResponseBuilder.buildFailureResponse(HttpStatus.BAD_REQUEST, "400",
                     "Failed to save contents in the file system! Reason: " + ex.getMessage());
         }
-        return ResponseBuilder.buildSuccessResponse(HttpStatus.OK, Map.of("contentIds", contentIds));
-    }
-
-    @GetMapping
-    public ResponseEntity<CustomHttpResponse> getContentsInfo(@RequestParam ArrayList<UUID> contentIds) {
-        List<Content> contentList = contentService.getContentsInfo(contentIds);
-        return ResponseBuilder.buildSuccessResponse(HttpStatus.OK, Map.of("contentList", contentList));
+        return ResponseBuilder.buildSuccessResponse(HttpStatus.OK, Map.of("urlList", urlList));
     }
 
 }
--- content-delivery-service/src/main/java/com/devrezaur/content/delivery/module/service/ContentService.java
+++ content-delivery-service/src/main/java/com/devrezaur/content/delivery/module/service/ContentService.java
@@ -1,7 +1,5 @@
 package com.devrezaur.content.delivery.module.service;
 
-import com.devrezaur.content.delivery.module.model.Content;
-import com.devrezaur.content.delivery.module.repository.ContentRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,7 +17,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class ContentService {
@@ -31,31 +28,24 @@ public class ContentService {
 
     private static Path storageLocation;
 
-    private final ContentRepository contentRepository;
-
-    public ContentService(ContentRepository contentRepository) {
-        this.contentRepository = contentRepository;
-    }
-
     @PostConstruct
     private void init() throws IOException {
         storageLocation = Paths.get(fileUploadPath).toAbsolutePath().normalize();
         Files.createDirectories(storageLocation);
     }
 
-    public List<UUID> saveContents(MultipartFile[] contents) throws Exception {
-        List<UUID> contentIdList = new ArrayList<>();
+    public List<String> saveContents(MultipartFile[] contents) throws Exception {
+        List<String> urlList = new ArrayList<>();
         if (!isContentListValid(contents)) {
             throw new Exception("Please provide valid contents for upload!");
         }
         for (MultipartFile content : contents) {
             String extension = StringUtils.getFilenameExtension(content.getOriginalFilename());
             String hash = generateHash(content.getName(), content.getContentType(), content.getSize());
-            contentIdList.add(saveContentInfoToDB(content.getName(), content.getContentType(), extension,
-                    content.getSize(), hash));
+            urlList.add(fileUploadPath + "/" + hash + "." + extension);
             storeContentToFileSystem(content, hash);
         }
-        return contentIdList;
+        return urlList;
     }
 
     private boolean isContentListValid(MultipartFile[] contents) {
@@ -72,24 +62,6 @@ public class ContentService {
         MessageDigest messageDigest = MessageDigest.getInstance("MD5");
         messageDigest.update(transformedName.getBytes(StandardCharsets.UTF_8));
         return new BigInteger(1, messageDigest.digest()).toString(RADIX);
-    }
-
-    private UUID saveContentInfoToDB(String contentName, String mimeType, String extension, long size, String hash) {
-        try {
-            Content content = contentRepository.save(new Content(null, contentName, mimeType, extension, size, hash));
-            return content.getContentId();
-        } catch (Exception ex) {
-            return null;
-        }
-    }
-
-    public List<Content> getContentsInfo(ArrayList<UUID> contentIds) {
-        List<Content> contentList = new ArrayList<>();
-        for (UUID contentId : contentIds) {
-            Content content = contentRepository.findByContentId(contentId);
-            contentList.add(content);
-        }
-        return contentList;
     }
 
     private void storeContentToFileSystem(MultipartFile content, String hash) throws IOException {
