e542a29e36ab810a81346eda4755cc891fc16a1d
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-10-04 15:50:15 +0600

    Updated RestTemplateConfiguration.java

--- common-module/src/main/java/com/devrezaur/common/module/config/RestTemplateConfiguration.java
+++ common-module/src/main/java/com/devrezaur/common/module/config/RestTemplateConfiguration.java
@@ -1,14 +1,19 @@
 package com.devrezaur.common.module.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RestTemplateConfiguration {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofMillis(3000))
+                .build();
     }
 }
