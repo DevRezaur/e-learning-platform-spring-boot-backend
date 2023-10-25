33d92fa52168e4f35863fb7aef8730cc4e3902fc
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-10-25 14:16:28 +0600

    Refactored code

--- common-module/src/main/java/com/devrezaur/common/module/util/KeycloakRoleConverter.java
+++ common-module/src/main/java/com/devrezaur/common/module/util/KeycloakRoleConverter.java
@@ -1,4 +1,4 @@
-package com.devrezaur.user.service.config;
+package com.devrezaur.common.module.util;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
@@ -6,7 +6,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
@@ -14,17 +13,13 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class KeycloakRoleConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
-
     @Override
     public AbstractAuthenticationToken convert(Jwt jwt) {
-        return new JwtAuthenticationToken(jwt, Stream.concat(new JwtGrantedAuthoritiesConverter().convert(jwt)
-                .stream(), extractRoles(jwt).stream()).collect(Collectors.toSet()));
+        return new JwtAuthenticationToken(jwt, extractRoles(jwt));
     }
-
 
     private Collection<GrantedAuthority> extractRoles(Jwt jwt) {
         Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm_access");
--- user-service/src/main/java/com/devrezaur/user/service/config/WebSecurityConfiguration.java
+++ user-service/src/main/java/com/devrezaur/user/service/config/WebSecurityConfiguration.java
@@ -1,8 +1,9 @@
 package com.devrezaur.user.service.config;
 
+import com.devrezaur.common.module.util.KeycloakRoleConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,17 +11,20 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth ->
-                        auth
-                                .requestMatchers(new AntPathRequestMatcher("/user", HttpMethod.GET.toString())).hasRole("USER")
-                                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-                                .anyRequest().fullyAuthenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(new KeycloakRoleConverter())));
-        return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(requestMatcherRegistry -> requestMatcherRegistry
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                        .anyRequest().fullyAuthenticated()
+                )
+                .oauth2ResourceServer(resourceServerConfigurer -> resourceServerConfigurer
+                        .jwt(jwtConfigurer -> jwtConfigurer
+                                .jwtAuthenticationConverter(new KeycloakRoleConverter()))
+                )
+                .build();
     }
 }
