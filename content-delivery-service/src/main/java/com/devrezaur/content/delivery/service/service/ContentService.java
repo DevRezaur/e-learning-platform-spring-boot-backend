199d038c7f6a79a79f447d430e2d440c0dde12d6
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-11-03 10:29:34 +0600

    Implemented resource-server security for content-delivery-service

--- content-delivery-service/src/main/java/com/devrezaur/content/delivery/service/service/ContentService.java
+++ content-delivery-service/src/main/java/com/devrezaur/content/delivery/service/service/ContentService.java
@@ -1,4 +1,4 @@
-package com.devrezaur.content.delivery.module.service;
+package com.devrezaur.content.delivery.service.service;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,6 +19,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Service class for storing contents to the file system storage.
+ * <p>
+ * This class provides methods for interacting with file server storage.
+ *
+ * @author Rezaur Rahman
+ */
 @Service
 public class ContentService {
 
@@ -28,12 +35,24 @@ public class ContentService {
     private static final int RADIX = 16;
     private static Path storageLocation;
 
+    /**
+     * Initializes the storage location for content files during bean creation.
+     *
+     * @throws IOException if an error occurs while creating the storage directory.
+     */
     @PostConstruct
     private void init() throws IOException {
         storageLocation = Paths.get(fileUploadPath).toAbsolutePath().normalize();
         Files.createDirectories(storageLocation);
     }
 
+    /**
+     * Saves the provided content files to the file system and generates unique URLs for each content.
+     *
+     * @param contents an array of multipart file objects representing the content files to be saved.
+     * @return list of unique URLs for the saved content files.
+     * @throws Exception if the provided content list is empty or invalid.
+     */
     public List<String> saveContents(MultipartFile[] contents) throws Exception {
         List<String> urlList = new ArrayList<>();
         if (!isContentListValid(contents)) {
@@ -48,6 +67,12 @@ public class ContentService {
         return urlList;
     }
 
+    /**
+     * Checks if the provided content list is valid and not empty.
+     *
+     * @param contents an array of multipart file objects.
+     * @return true if the content list is valid and not empty. Otherwise, returns false.
+     */
     private boolean isContentListValid(MultipartFile[] contents) {
         for (MultipartFile content : contents) {
             if (content == null || content.isEmpty()) {
@@ -57,6 +82,15 @@ public class ContentService {
         return true;
     }
 
+    /**
+     * Generates a unique hash for a content file based on its name, MIME type, size, and timestamp.
+     *
+     * @param contentName the name of the content file.
+     * @param mimeType    the MIME type of the content.
+     * @param size        the size of the content.
+     * @return an unique hash as a string.
+     * @throws NoSuchAlgorithmException if an error occurs during hash generation.
+     */
     private String generateHash(String contentName, String mimeType, long size) throws NoSuchAlgorithmException {
         String transformedName = contentName + mimeType + size + new Date().getTime() +
                 ThreadLocalRandom.current().nextInt();
@@ -65,6 +99,13 @@ public class ContentService {
         return new BigInteger(1, messageDigest.digest()).toString(RADIX);
     }
 
+    /**
+     * Stores the content file to the file system using its unique hash as the filename.
+     *
+     * @param content the multipart file representing the content file.
+     * @param hash    the unique hash used as the filename.
+     * @throws IOException if an error occurs during file storage.
+     */
     private void storeContentToFileSystem(MultipartFile content, String hash) throws IOException {
         Path targetLocation = storageLocation.resolve(hash);
         Files.copy(content.getInputStream(), targetLocation);
