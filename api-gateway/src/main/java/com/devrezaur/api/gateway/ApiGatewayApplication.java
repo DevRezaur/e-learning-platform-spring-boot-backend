6440b18a6e94d79c70c6337d0f2250b472e2c109
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-11-06 16:17:48 +0600

    Added api-gateway service

---
+++ api-gateway/src/main/java/com/devrezaur/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,27 @@
+package com.devrezaur.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * The main application class for 'api-gateway'.
+ * <p>
+ * This class serves as the bootstrap for the API Gateway.
+ * Which is responsible for routing requests to different microservices within the system.
+ *
+ * @author Rezaur Rahman
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    /**
+     * Main method that starts the 'api-gateway' application.
+     *
+     * @param args command line arguments passed to the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
