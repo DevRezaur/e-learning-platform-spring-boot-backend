b713e6d484ff20efcc7d692dc1a5c9bcbaeb3015
Rezaur Rahman <rezaur.official@gmail.com>
2024-02-21 20:08:51 +0600

    Added CoursePageController

--- api-gateway/src/main/java/com/devrezaur/api/gateway/controller/CoursePageController.java
+++ api-gateway/src/main/java/com/devrezaur/api/gateway/controller/CoursePageController.java
@@ -3,30 +3,33 @@ package com.devrezaur.api.gateway.controller;
 import com.devrezaur.api.gateway.service.CourseAPIService;
 import com.devrezaur.common.module.model.CustomHttpResponse;
 import com.devrezaur.common.module.util.ResponseBuilder;
+import jakarta.annotation.Nullable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/home-screen-api")
-public class HomeScreenController {
+@RequestMapping("/course-page-api")
+public class CoursePageController {
 
     private final CourseAPIService courseAPIService;
 
-    public HomeScreenController(CourseAPIService courseAPIService) {
+    public CoursePageController(CourseAPIService courseAPIService) {
         this.courseAPIService = courseAPIService;
     }
 
     @GetMapping
-    public ResponseEntity<CustomHttpResponse> getHomeScreenData() {
+    public ResponseEntity<CustomHttpResponse> getCourseList(@RequestParam @Nullable Integer pageNumber,
+                                                            @RequestParam @Nullable Integer limit) {
         List<Map<String, Object>> courseList;
         try {
-            courseList = courseAPIService.getAllCourses(0, 6);
+            courseList = courseAPIService.getAllCourses(pageNumber, limit);
         } catch (Exception ex) {
             return ResponseBuilder.buildFailureResponse(HttpStatus.BAD_REQUEST, "400",
                     "Failed to fetch course list! Reason: " + ex.getMessage());
