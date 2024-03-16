2adaca59f8f34073c96bc2b473c6ea9aeca9e3d7
Rezaur Rahman <rezaur.official@gmail.com>
2024-03-16 14:26:26 +0600

    Secured add course content API

--- course-management-service/src/main/java/com/devrezaur/course/management/service/controller/CourseContentController.java
+++ course-management-service/src/main/java/com/devrezaur/course/management/service/controller/CourseContentController.java
@@ -7,6 +7,7 @@ import com.devrezaur.course.management.service.service.CourseContentService;
 import jakarta.annotation.Nullable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,7 @@ public class CourseContentController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CustomHttpResponse> addCourseContent(@RequestBody CourseContent courseContent) {
         try {
             courseContentService.addCourseContent(courseContent);
