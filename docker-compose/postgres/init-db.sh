13489b316ab53ad676cd642a5643a89e5d5b2a20
Rezaur Rahman <rezaur.official@gmail.com>
2025-03-01 19:35:36 +0600

    Refactored docker compose file

--- docker-compose/postgres/init-db.sh
+++ docker-compose/postgres/init-db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'keycloak'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE keycloak"
 psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'user_service_db'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE user_service_db"
 psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'course_management_service_db'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE course_management_service_db"
